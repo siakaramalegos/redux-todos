88326703f0b590462cfee06533f655691f3f5aef
Sia <siakaramalegos@gmail.com>
2016-06-28 13:24:07 -0500

    Adds the store

--- app/app.js
+++ app/app.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import todoApp from './reducers';
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './actions';
+
+let store = createStore(todoApp);
+
+// Log initial state
+console.log(store.getState());
+
+// Every time state changes, log it
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
 
 class App extends React.Component {
   render () {
--- app/reducers.js
+++ app/reducers.js
@@ -1,11 +1,12 @@
-import { VisibilityFilters } from './actions';
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters
+} from './actions';
+import { combineReducers } from 'redux';
 
-const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
-};
-
-function todos (state = [], action) {
+const todos = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       return [
@@ -14,23 +15,22 @@ function todos (state = [], action) {
             text: action.text,
             completed: false
           }
-        ]
-      });
+        ];
     case TOGGLE_TODO:
       return state.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed
-            })
-          }
-          return todo;
-        };
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo;
+      });
     default:
       return state;
   }
 }
 
-function visibilityFilter(state = SHOW_ALL, action) {
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter;
@@ -39,9 +39,16 @@ function visibilityFilter(state = SHOW_ALL, action) {
   }
 }
 
-function todoAPP (state = {}, action) {
-  return {
-    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-    todos: todos(state.todos, action)
-  };
-}
+// Combine Reducers basically does this (assumes boilerplate followed)
+// function todoAPP (state = {}, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   };
+// }
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+});
+
+export default todoApp;
